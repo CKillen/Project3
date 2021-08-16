46c6636898e3292ce692c633a1b27df80fbebcf1
Chris K <cl_killen@hotmail.com>
2021-08-15 20:38:20 -0400

    Forcing early phaser

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -5,16 +5,21 @@ export const Form = ({ onSubmit, formBtntext, loginSignupState }) => {
   return (
     <form onSubmit={onSubmit}>
       <div className="form-group usernameTitle">
-        <label htmlFor="name">Username</label>
-        <input className="form-control usernameBox" id="name" />
+        <label htmlFor="name">Username (between 3 - 10 characters)</label>
+        <input 
+          className="form-control usernameBox" 
+          id="name" 
+          pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{3,10}$"
+        />
       </div>
       <div className="form-group passwordTitle">
-        <label htmlFor="password">Password</label>
+        <label htmlFor="password">Password (between 6 - 24)</label>
         <input
           type="password"
           className="form-control passwordBox"
           id="password"
           placeholder=""
+          pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{6,24}$"
         />
       </div>
       <div className="form-group">
