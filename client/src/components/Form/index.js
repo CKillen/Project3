ab23031f1e6d300e0ffb71ea26cd8ace7777ac11
Chris K <cl_killen@hotmail.com>
2021-08-15 21:02:40 -0400

    regex right, no clue on missing ship

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -9,7 +9,7 @@ export const Form = ({ onSubmit, formBtntext, loginSignupState }) => {
         <input 
           className="form-control usernameBox" 
           id="name" 
-          pattern="^{3,10}$"
+          pattern="^[a-zA-Z0-9]{3,10}$"
         />
       </div>
       <div className="form-group passwordTitle">
@@ -19,7 +19,7 @@ export const Form = ({ onSubmit, formBtntext, loginSignupState }) => {
           className="form-control passwordBox"
           id="password"
           placeholder=""
-          pattern="^(?=.*[a-z])(?=.*[A-Z]).{6,24}$"
+          pattern="^(?=.*[a-z]).{6,24}$"
         />
       </div>
       <div className="form-group">
